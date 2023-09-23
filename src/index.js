9cc8852afb629122b12d0625c7f9394326c3ba08
Ivan <iaranda264@gmail.com>
2023-09-22 23:58:26 -0300

    configurando socket io en servidor

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import mongoose from "mongoose";
 import userRouter from "./routes/users.routes.js";
 import prodsRouter from "./routes/products.routes.js";
 import cartsRouter from "./routes/carts.routes.js";
+import { Server } from "socket.io";
 
 const app = Express();
 const PORT=8080;
 
-mongoose.connect(`mongodb+srv://ivnarnd:@cluster0.3ykyt9n.mongodb.net/?retryWrites=true&w=majority`).then(()=>console.log('DB is connected')).catch((error)=>console.log('Error in connection',error));
+const server = app.listen(PORT,()=>console.log(`Servidor corriendo en puerto:${PORT}`));
 
+const io = new Server(server);
+
+io.on('connection',()=>{
+    console.log('Servidor socket io conectado');
+});
+mongoose.connect(`mongodb+srv://ivnarnd:@cluster0.3ykyt9n.mongodb.net/?retryWrites=true&w=majority`).then(()=>console.log('DB is connected')).catch((error)=>console.log('Error in connection',error));
 app.use(Express.json());
+
 app.use('/api/users',userRouter);
 app.use('/api/products',prodsRouter);
 app.use('/api/carts',cartsRouter);
 
-app.listen(PORT,()=>console.log(`Servidor corriendo en puerto:${PORT}`));
