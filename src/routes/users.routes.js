baf16021d45cb2e1b0ece3a7ce33063a5e90833a
Ivan Aranda <iaranda264@gmail.com>
2024-02-02 07:02:35 -0300

    agregando las rutas de reestablecimiento de contraseña

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { deleteUser, getUser, getUsers, postUser, putUser } from "../controllers/users.controller.js";
+import { deleteUser, getUser, getUsers, postUser, putUser, resetPassword, sendRecoverylink } from "../controllers/users.controller.js";
 import { passportError, authorization } from "../utils/messagesError.js";
+import crypto from 'crypto';//para generar el token de 1hr de reset
+
 const userRouter = Router();
 
 userRouter.get('/',passportError('jwt'),authorization('admin'),getUsers);
@@ -9,4 +11,8 @@ userRouter.post('/',postUser);
 userRouter.put('/:id',putUser);
 userRouter.delete('/:id',deleteUser);
 
+
+//rutas para la recuperacion de contraseña
+userRouter.post('/password-recovery',sendRecoverylink);
+userRouter.post('/reset-password/:token',resetPassword);
 export default userRouter;
